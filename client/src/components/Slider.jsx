40b552639557a8388015d247072b87353af6c2ca
Ali <syedmohtashimali1910@gmail.com>
2024-01-01 20:35:42 +0530

    Slider pe kaam

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Image1 from '../assets/slider_images/Image1.jpg'
+import Image2 from '../assets/slider_images/Image2.jpg'
+import Image3 from '../assets/slider_images/Image3.jpg'
+import Image4 from '../assets/slider_images/Image4.jpg'
 
 const Slider = () => {
-  return <>aaaaaaaaa</>;
+  return (
+    <div className="slider-container">
+      <img src={Image1} alt="Slider Image 1" />
+      <img src={Image2} alt="Slider Image 2" />
+      <img src={Image3} alt="Slider Image 3" />
+      <img src={Image4} alt="Slider Image 4" />
+    </div>
+  );
 };
 
 export default Slider;
